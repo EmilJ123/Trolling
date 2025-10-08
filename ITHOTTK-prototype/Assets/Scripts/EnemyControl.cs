8aac7b48717088b8ff8926f265ad12f26d35fc6c
EmilJ123 <ewaltoftolsen@gmail.com>
2025-10-08 10:52:35 +0200

    wtf2

---
+++ ITHOTTK-prototype/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public Transform[] wayPoints;
+    public int targetPoint;
+
+    public float moveSpeed;
+
+    float waitTime;
+    [SerializeField] float startWaitTime;
+    public bool isWaiting;
+
+    void Start()
+    {
+        targetPoint = 0;
+        waitTime = startWaitTime;
+    }
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[targetPoint].position, moveSpeed *Time.deltaTime);
+
+        if (transform.position == wayPoints[targetPoint].position)
+        {
+            if (waitTime <= 0)
+            {
+                targetPoint++;
+                if(targetPoint >= wayPoints.Length) targetPoint = 0;
+                waitTime = startWaitTime;
+                isWaiting = false;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+                isWaiting = true;
+            }
+        }
+            
+    }
+}
